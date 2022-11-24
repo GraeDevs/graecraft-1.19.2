d23a2d70c01865bd7a3e5fc2ac02a187235ffbf3
Renk <cadenfrizellgaming@gmail.com>
2022-11-24 15:52:21 -0600

    Initial commit

---
+++ src/main/java/net/graedevs/graecraft/GraecraftClient.java
@@ -0,0 +1,10 @@
+package net.graedevs.graecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GraecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
